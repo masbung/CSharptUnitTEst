8eb7ffab43f746a8676c8d253b6d130caf1d2b57
mag96 <masbung@sistrategia.com>
2019-03-24 13:20:27 -0600

    Candies problem

--- src/CSharpUnitTest/Program.cs
+++ src/CSharpUnitTest/Program.cs
@@ -9,9 +9,37 @@ namespace CSharpUnitTest
     class Program
     {
         static void Main(string[] args) {
-            var mathCalculator = new MathCalculator();
-            var sum = mathCalculator.Add(3, 5);
-            Console.WriteLine(sum.Value);
+            //var mathCalculator = new MathCalculator();
+            //var sum = mathCalculator.Add(3, 5);
+            //Console.WriteLine(sum.Value);
+
+            int candies = 865;
+            int bigBagCapacity = 25;
+            int smallBagCapacity = 10;
+
+            decimal firstDivision = candies / bigBagCapacity;
+
+            var totalBigBags = Math.Truncate(firstDivision);
+            Console.WriteLine("Total big bags: " + totalBigBags);
+
+            //var remainCandies = candies - (totalBigBags * bigBagCapacity);
+            //Console.WriteLine("Remain candies: " + remainCandies);
+
+            var remainCandies = candies % bigBagCapacity;
+            Console.WriteLine("Remain candies: " + remainCandies);
+
+            var totalSmallBags = remainCandies / smallBagCapacity;
+            Console.WriteLine("Total small bags: " + totalSmallBags);
+
+            remainCandies = remainCandies % smallBagCapacity;
+            Console.WriteLine("Remain candies: " + remainCandies);
+
+            Console.WriteLine("Ceiling");
+            decimal remainCandiesTwo = candies % bigBagCapacity;
+            var totalSmallBagsTwo = Math.Ceiling(remainCandiesTwo / 10);
+
+            Console.WriteLine("Total small bags two: " + totalSmallBagsTwo);
+
         }
     }
 }
